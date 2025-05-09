6be785ab282980c4fe0ac540ae2484ea6879139d
learningenglish-dev <luutrutailieucuathu2@gmail.com>
2025-05-09 15:01:48 +0000

    Initial commit

---
+++ exercise-data.js
@@ -0,0 +1,55 @@
+// exercise-data.js
+const exerciseData = [
+  {
+    id: 1,
+    sentence: `<input type="text" id="gap1"> with problems = 
+               <input type="text" id="gap2"> challenges = 
+               <input type="text" id="gap3"> difficulties`,
+    answers: {
+      gap1: "deal",
+      gap2: "confront",
+      gap3: "mitigate"
+    }
+  },
+  {
+    id: 2,
+    sentence: `get <input type="text" id="gap4"> = develop health 
+               <input type="text" id="gap5"> = 
+               <input type="text" id="gap6"> to ailments`,
+    answers: {
+      gap4: "sick",
+      gap5: "complications",
+      gap6: "succumb"
+    }
+  },
+  {
+    id: 3,
+    sentence: `do exercise = participate in fitness 
+               <input type="text" id="gap7"> = engage in 
+               <input type="text" id="gap8"> activity`,
+    answers: {
+      gap7: "regimens",
+      gap8: "physical"
+    }
+  },
+  {
+    id: 4,
+    sentence: `spend money on = 
+               <input type="text" id="gap9"> funds to = 
+               <input type="text" id="gap10"> expenditures on`,
+    answers: {
+      gap9: "allocate",
+      gap10: "disburse"
+    }
+  },
+  {
+    id: 5,
+    sentence: `stop crime = prevent
+               <input type="text" id="gap11"> behavior = 
+               <input type="text" id="gap12"> illicit activities`,
+    answers: {
+      gap11: "criminal",
+      gap12: "suppress"
+    }
+  }
+];
---
+++ vocabulary-data.js
@@ -0,0 +1,23 @@
+// vocabulary-data.js
+const vocabularyData = [
+  {
+    words: ["deal with problems", "confront challenges", "mitigate difficulties"],
+    gaps: ["deal", "confront", "mitigate"]
+  },
+  {
+    words: ["get sick", "develop health complications", "succumb to ailments"],
+    gaps: ["sick", "complications", "succumb"]
+  },
+  {
+    words: ["do exercise", "participate in fitness regimens", "engage in physical activity"],
+    gaps: ["regimens", "physical"]
+  },
+  {
+    words: ["spend money on", "allocate funds to", "disburse expenditures on"],
+    gaps: ["allocate", "disburse"]
+  },
+  {
+    words: ["stop crime", "prevent criminal behavior", "suppress illicit activities"],
+    gaps: ["criminal", "suppress"]  
+  }
+];
